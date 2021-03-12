8f0e538f33082b36c4a6528409697e13ea99ae4d
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2021-03-12 17:00:36 +0100

    Add Restaurant

---
+++ database/seeds/RestaurantSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Restaurant;
+use Illuminate\Database\Seeder;
+Use Faker\Generator as Faker;
+
+class RestaurantSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 20; $i++) { 
+            $newRestaurant = new Restaurant();
+            $newRestaurant->name = $faker->sentence(2);
+            $newRestaurant->address = $faker->address();
+            $newRestaurant->phone_number = $faker->phoneNumber();
+            $newRestaurant->save();
+        }
+    }
+}
