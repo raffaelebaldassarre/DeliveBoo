4df88e1a0b0ed5c6e88eb548854743fb1a815b86
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2021-03-16 19:36:29 +0100

    add delete in dishcontroller, and fix some bugs

--- routes/web.php
+++ routes/web.php
@@ -22,14 +22,15 @@ Auth::routes();
 Route::middleware('auth')->namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
     Route::get('/', 'HomeController@index')->name('index');
     Route::resource('restaurants', 'RestaurantController');
-    /* Route::resource('restaurant/{slug}/dishes', 'DishController'); */
-    Route::get('restaurants/{slug}/dishes', 'DishController@index')->name('dishes.index');
+/*     Route::resource('restaurant/{slug}/dishes', 'DishController');
+ */    Route::get('restaurants/{slug}/dishes', 'DishController@index')->name('dishes.index');
     Route::get('restaurants/{slug}/dishes/create', 'DishController@create')->name('dishes.create');
     Route::post('restaurants/dishes', 'DishController@store')->name('dishes.store');
     Route::get('restaurants/{slug}/dishes/{dish}', 'DishController@show')->name('dishes.show');
     Route::get('restaurants/{slug}/dishes/{dish}/edit', 'DishController@edit')->name('dishes.edit');
     Route::put('restaurants/dishes/{dish}', 'DishController@update')->name('dishes.update');
     Route::patch('restaurants/dishes/{dish}', 'DishController@update')->name('dishes.update');
+    Route::delete('restaurants/dishes/{dish}', 'DishController@destroy')->name('dishes.destroy');
 });
 
 
