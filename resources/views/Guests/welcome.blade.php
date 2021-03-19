930a27f61bd69b6b19ea7f3b5cd89e6eb6ece864
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2021-03-19 11:58:04 +0100

    Add CartController & CategorySeeder

--- resources/views/Guests/welcome.blade.php
+++ resources/views/Guests/welcome.blade.php
@@ -6,19 +6,17 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-lg-3 mb-4">
-            <h1 class="mt-4">Filters</h1>
-
-            <h3 class="mt-2">Categories</h3>
-            <div class="form-check" v-for="(category, index) in categories">
+        <div class="col-lg-12 mb-4">
+            <h3 class="mt-2">Scegli tra le categorie</h3>
+            <div class="form-check d-inline-flex" v-for="(category, index) in categories">
                 <input class="form-check-input" type="checkbox" :value="category.id" :id="'category'+index" v-model="categories_rest">
                 <label class="form-check-label" :for="'category' + index">
                     @{{ category.name }}
                 </label>
             </div>
-            <h3>La tua ricerca ha trovato: @{{restaurants.length}} ristoranti</h3>
         </div>
-        <div class="col-lg-9">
+        <div class="col-lg-12">
+            <h3>La tua ricerca ha trovato: @{{restaurants.length}} ristoranti</h3>
             <div class="row mt-4">
                 <div class="col-lg-4 col-md-6 mb-4" v-for="restaurant in restaurants">
                     <div class="card h-100">
