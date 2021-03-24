f37184c1f5b9e0d55d488c627bbf1397cf8dc8cd
williamtruppi <williamt@hotmail.it>
2021-03-24 10:14:46 +0100

    validazione ordine quasi completa

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 use App\Restaurant;
+use App\Order;
 use Illuminate\Http\Request;
+use Faker\Generator as Faker;
 
 class CartController extends Controller
 {
@@ -12,13 +14,45 @@ class CartController extends Controller
         return view('cart.index', compact('restaurant'));
     }
 
-    public function store(Request $request){
-        dd($request);
+    public function store(Request $request, Faker $faker){
+        /* dd($request); */
         $dishesList = [];
+        $quantityList = [];
         $cookieCart = json_decode($_COOKIE["cookieCart"]);
+        /* dd($cookieCart); */
+        $restaurant_id = $cookieCart[0]->restaurant_id;
+        /* dd($restaurant_id); */
+        $request["restaurant_id"] = $restaurant_id;
+        $request["exp_date"] = $faker->dateTimeInInterval($startDate = 'now', $endDate = '+ 1 hour');
+        /* dd($request->all()); */
         foreach ($cookieCart as $cook) {
             array_push ($dishesList , $cook->id);
+            array_push ($quantityList , $cook->quantity);
         }
+        /* dd($quantityList); */
+
+        
+        $validateData = $request->validate([
+            'special_requests' => 'nullable',
+            'name' => 'required',
+            'lastname' => 'required',
+            'address' => 'required',
+            'phone_number' => 'required',
+            'payment_id' => 'nullable',
+            'restaurant_id' => 'required|exists:restaurants,id',
+            'dishes' => 'exists:dishes,id',
+            'exp_date' => '',
+            'quantity' => '',
+        ]);
+        
+        /* dd($validateData); */
+
+        Order::create($validateData);
+
+        $new_order = Order::orderBy("id", "desc")->first();
+
+        $new_order->dishes()->attach($dishesList, $quantityList);
+        /* $new_order->dishes()->sync(); */
         
         
      }
