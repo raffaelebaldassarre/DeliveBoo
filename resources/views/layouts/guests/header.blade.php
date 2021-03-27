a15f70add62995b3d3edda892ab7b67a0af8aa8c
Loris Matteri <loris.matteri93@gmail.com>
2021-03-27 12:55:52 +0100

    add animation to header's texts

--- resources/views/layouts/guests/header.blade.php
+++ resources/views/layouts/guests/header.blade.php
@@ -1,5 +1,5 @@
 <header>
-    <div class="deliveboo">
+    <div class="deliveboo position-relative">
         <h1>Deliveboo</h1>
         <h4>Delicious food for every mood</h4>
         <p>Il ristorante che vuoi, a casa tua.</p>
