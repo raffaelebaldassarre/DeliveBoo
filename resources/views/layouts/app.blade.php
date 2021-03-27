8ee284ed3d0e37d5b8f15e645ec22ef6ed846def
williamtruppi <williamt@hotmail.it>
2021-03-27 12:05:56 +0100

    footer added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,26 +1,24 @@
 @include('layouts.guests.head')
 @include('layouts.guests.navbar')
-@include('layouts.guests.header')
+@yield('header')
 
-
-    <div id="app">
-
-
-        
-
-        <main class="py-4">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        @yield('content')
-                    </div>
+<div id="app">
+    <main class="py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    @yield('content')
                 </div>
             </div>
-        </main>
-    </div>
+        </div>
+    </main>
+</div>
 
-    @yield('payment')
-    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script> --}}
+@yield('payment')
+{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script> --}}
+
+@include('layouts.guests.footer')
+
+
     
-</body>
-</html>
+    
