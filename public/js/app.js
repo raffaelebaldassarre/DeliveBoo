d827a27a555430c8d64fc945862cb78e2c642dc7
Loris Matteri <loris.matteri93@gmail.com>
2021-03-27 17:33:22 +0100

    fix header animation responsive

--- public/js/app.js
+++ public/js/app.js
@@ -50183,8 +50183,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Mariapia\Desktop\ProgettoFinale\DeliveBoo\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Mariapia\Desktop\ProgettoFinale\DeliveBoo\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Boolean23\8_PROGETTO FINALE\DeliveBoo\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Boolean23\8_PROGETTO FINALE\DeliveBoo\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
