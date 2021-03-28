4edeff2f3e9baf5eb155b7cc8583ca17a929e9ea
Mariapia Gianguzzi <piagianguzzi@gmail.com>
2021-03-28 18:08:43 +0200

    commit navbar fixed

--- public/js/app.js
+++ public/js/app.js
@@ -50180,8 +50180,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\willi\Desktop\Boolean\DeliveBoo\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\willi\Desktop\Boolean\DeliveBoo\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Mariapia\Desktop\ProgettoFinale\DeliveBoo\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Mariapia\Desktop\ProgettoFinale\DeliveBoo\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
