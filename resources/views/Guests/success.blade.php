18cc9daf927babc4924320206a036b912b51bc40
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2021-03-29 09:40:48 +0200

    Fix success blade

--- resources/views/Guests/success.blade.php
+++ resources/views/Guests/success.blade.php
@@ -2,10 +2,9 @@
 
 @section('content')
 <div id="success_container" class="text-center col-lg-12">
-    <img src="http://127.0.0.1:8000/images/logogif.gif" alt=""> 
+    <img src="{{asset('images/logogif.gif')}}" alt=""> 
         <h1 class="my-4">Grazie per il tuo ordine!</h1> 
-        <p>L'ordine eseguito n° 36 è andato a buon fine.</p> 
-        <p><strong>Controlla nella tua mail.</strong></p> 
-        <a href="http://127.0.0.1:8000" class="btn btn-lg btn-outline font-weight-bold p-4">Torna all'home page per mangiare ancora!</a>
+        <p>A breve riceverai una e-mail con il riepilogo dell'ordine.</p> 
+        <a href="{{ url('/') }}" class="btn btn-lg btn-outline font-weight-bold p-4">Torna all'home page per effettuare un nuovo ordine</a>
     </div>
 @endsection
