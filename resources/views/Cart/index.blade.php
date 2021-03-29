4f686ca907ca29902a8ba0dd81bed4fae8ce8944
Loris Matteri <loris.matteri93@gmail.com>
2021-03-29 13:27:35 +0200

    checkout page

--- resources/views/Cart/index.blade.php
+++ resources/views/Cart/index.blade.php
@@ -1,141 +1,153 @@
 @extends('layouts.app')
 
 @section('content')
-    <table class="table">
-        <h3 id="rest_id" class="d-none">{{ $restaurant->id }}</h3>
-        <thead>
-            <tr>
-                <th>                    
-                    Piatto
-                </th>
-                <th>Quantità</th>
-                <th>Prezzo totale per piatto</th>
-            </tr>
-        </thead>
-        <tbody v-for="(item, index) in orderCart">
-            <tr>
-                <td scope="row">@{{item.name}}</td>
-                <td>@{{item.quantity}}</td>
-                <td>@{{item.totalDishPrice}} €</td>
-                <td>                            
-                    <button id="minus" class="btn btn-outline-primary" @click="minusDish(item)">-</button>
-                    <button id="plus" class="btn btn-outline-primary" @click="moreDish(item)">+</button>
-                    <button id="trash" @click="deleteDish(index)" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-    <h3 v-if="totalPrice != 0">Prezzo Totale: @{{totalPrice}} €</h3>
+
+    <div class="text-center col-lg-6">
+        <div id="app">
+            <div class="receipt_container">
+                <h3 id="rest_id" class="d-none">{{ $restaurant->id }}</h3>
+                <h2>Riepilogo Ordine:</h2>
+                <ul>
+                    <li v-for="(item, index) in orderCart">@{{item.quantity}}x @{{item.name}} &rArr; @{{item.totalDishPrice}} €</li>
+                </ul>
+                {{-- <table class="table">
+                    <thead>
+                        <tr>
+                            <th>                    
+                                Piatto
+                            </th>
+                            <th>Quantità</th>
+                            <th>Prezzo totale per piatto</th>
+                        </tr>
+                    </thead>
+                    <tbody v-for="(item, index) in orderCart">
+                        <tr>
+                            <td scope="row">@{{item.name}}</td>
+                            <td>@{{item.quantity}}</td>
+                            <td>@{{item.totalDishPrice}} €</td>
+                            <td>                            
+                                <button id="minus" class="btn btn-outline-primary" @click="minusDish(item)">-</button>
+                                <button id="plus" class="btn btn-outline-primary" @click="moreDish(item)">+</button>
+                                <button id="trash" @click="deleteDish(index)" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table> --}}
+                <h2 v-if="totalPrice != 0">Prezzo Totale: @{{totalPrice}} €</h2>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('payment')
 
-<div class="container">
-    <div class="col-lg-6 p-0">
-        <h3 class="py-3">Inserisci i tuoi dati per la consegna</h3>
-        <form action="{{route('cart.store')}}" method="post" id="payment-order-user" name="payment-order-user" onsubmit="testform">
+    <div class="col-lg-6">
+        <div class="receipt_container text-center bg-white">
+            <h3 class="text-center py-3">Inserisci i tuoi dati per la consegna</h3>
+            <form action="{{route('cart.store')}}" method="post" id="payment-order-user" name="payment-order-user" onsubmit="testform">
     
-            @csrf
+                @csrf
     
-            {{-- NOME --}}
-            <div class="form-group">
-                <label for="name">NOME</label>
-                <input class="form-control" type="text" name="name" id="name" value="{{old('name')}}">
-            </div>
-            @error('name')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-    
-            {{-- COGNOME --}}
-            <div class="form-group">
-                <label for="lastname">COGNOME</label>
-                <input class="form-control" type="text" name="lastname" id="lastname" value="{{old('lastname')}}">
-            </div>
-            @error('lastname')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-    
-            {{-- INDIRIZZO --}}
-            <div class="form-group">
-                <label for="address">INDIRIZZO</label>
-                <input class="form-control" type="text" name="address" id="address" value="{{old('address')}}">
-            </div>
-            @error('address')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-    
-            {{-- NUMERO DI TELEFONO --}}
-            <div class="form-group">
-                <label for="phone_number">NUMERO DI TELEFONO</label>
-                <input class="form-control" type="text" name="phone_number" id="phone_number" value="{{old('phone_number')}}">
-            </div>
-            @error('phone_number')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-    
-            {{-- RICHIESTE SPECIALI --}}
-            <div class="form-group">
-                <label for="special_requests">RICHIESTE SPECIALI</label>
-                <input class="form-control" type="text" name="special_requests" id="special_requests" value="{{old('special_requests')}}">
-            </div>
-            @error('special_requests')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-
-            <div id="dropin-wrapper">
-                <div id="checkout-message"></div>
-                <div style="width:60%; margin:auto;">
-                  <div id="dropin-container"></div>
+                {{-- NOME --}}
+                <div class="form-group">
+                    <label for="name">NOME</label>
+                    <input class="form-control" type="text" name="name" id="name" value="{{old('name')}}">
                 </div>
-                <button style="background-color:yellow" id="submit-button">Submit payment</button>
-              </div>
-            
-            
-             <script>
-               
-               var form = document.getElementById('payment-order-user');
-               /* var button = document.querySelector('#submit-button'); */
-            
-               braintree.dropin.create({
-                 authorization: "sandbox_zjph3bx7_x2h6cngzkjxbdss9",
-                 container: '#dropin-container'
-               }, function (createErr, instance) {
-                 form.addEventListener('submit', function () {
-                    var name = document.getElementById("name").value;
-                    var lastname = document.getElementById("lastname").value;
-                    var phone_number = document.getElementById("phone_number").value;
-                    var address = document.getElementById("address").value;
-                    if (name === '' || lastname === '' || phone_number === '' || address === '')
-                    {
-                        alert("Inserisci tutti i dati richiesti per la spedizione");
-                        return false;
-                    } else {
-                        event.preventDefault();
-                    }
-                   instance.requestPaymentMethod(function (err, payload) {
-                     $.get('{{ route("payment.process") }}', {payload}, function (response) {
-                       if (response.success) {
-                         localStorage.clear();
-                         alert('Payment successfull!');
-                         form.submit();
-                       } else {
-                         alert('Payment failed');
-                       }
-                     }, 'json');
-                   });
-                 });
-               });
-               
-
-               function testform()
-                {
+                @error('name')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+    
+                {{-- COGNOME --}}
+                <div class="form-group">
+                    <label for="lastname">COGNOME</label>
+                    <input class="form-control" type="text" name="lastname" id="lastname" value="{{old('lastname')}}">
+                </div>
+                @error('lastname')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+    
+                {{-- INDIRIZZO --}}
+                <div class="form-group">
+                    <label for="address">INDIRIZZO</label>
+                    <input class="form-control" type="text" name="address" id="address" value="{{old('address')}}">
+                </div>
+                @error('address')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+    
+                {{-- NUMERO DI TELEFONO --}}
+                <div class="form-group">
+                    <label for="phone_number">NUMERO DI TELEFONO</label>
+                    <input class="form-control" type="text" name="phone_number" id="phone_number" value="{{old('phone_number')}}">
+                </div>
+                @error('phone_number')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+    
+                {{-- RICHIESTE SPECIALI --}}
+                <div class="form-group">
+                    <label for="special_requests">RICHIESTE SPECIALI</label>
+                    <input class="form-control" type="text" name="special_requests" id="special_requests" value="{{old('special_requests')}}">
+                </div>
+                @error('special_requests')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+    
+                <div id="dropin-wrapper">
+                    <div id="checkout-message"></div>
+                    <div {{-- style="width:60%; margin:auto;" --}}>
+                        <div id="dropin-container"></div>
+                    </div>
+                    <button id="submit-button" class="btn btn-lg btn-outline font-weight-bold p-4">Submit payment</button>
+                </div>
+                
+                
+                    <script>
                     
-                }
-             </script>
-            
-            {{-- <button type="submit" class="btn btn-outline-success btn-lg">Submit</button> --}}
-        </form>
+                    var form = document.getElementById('payment-order-user');
+                    /* var button = document.querySelector('#submit-button'); */
+                
+                    braintree.dropin.create({
+                        authorization: "sandbox_zjph3bx7_x2h6cngzkjxbdss9",
+                        container: '#dropin-container'
+                    }, function (createErr, instance) {
+                        form.addEventListener('submit', function () {
+                        var name = document.getElementById("name").value;
+                        var lastname = document.getElementById("lastname").value;
+                        var phone_number = document.getElementById("phone_number").value;
+                        var address = document.getElementById("address").value;
+                        if (name === '' || lastname === '' || phone_number === '' || address === '')
+                        {
+                            alert("Inserisci tutti i dati richiesti per la spedizione");
+                            return false;
+                        } else {
+                            event.preventDefault();
+                        }
+                        instance.requestPaymentMethod(function (err, payload) {
+                            $.get('{{ route("payment.process") }}', {payload}, function (response) {
+                            if (response.success) {
+                                localStorage.clear();
+                                alert('Payment successfull!');
+                                form.submit();
+                            } else {
+                                alert('Payment failed');
+                            }
+                            }, 'json');
+                        });
+                        });
+                    });
+                    
+    
+                    function testform()
+                    {
+                        
+                    }
+                    </script>
+                
+            </form>
+        </div>
     </div>
-</div>
     
 @endsection
+
+
--- resources/views/Guests/welcome.blade.php
+++ resources/views/Guests/welcome.blade.php
@@ -5,58 +5,61 @@
 @endsection
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 mb-4">
-                <h3 class="mt-2 text-center">Scegli tra queste categorie di cucina</h3>
-                
-                <!-- Top content -->
-                <div class="top-content">
-                    <div class="container-fluid">
-                        <div id="carousel-example" class="carousel slide" data-ride="carousel">
-                            <div class="carousel-inner row w-100 mx-auto" role="listbox">
-                                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3" :class="index==0 ? 'active' : '' " v-for="(category, index) in categories">
-                                    <input class="form-check-input d-none" type="checkbox" :value="category.id" :id="'category'+index" v-model="categories_rest">
-                                    <label class="form-check-label" :for="'category' + index">
-                                        <img src="//placehold.it/600x400/000/fff?text=1" class="img-fluid mx-auto d-block" alt="img1">
-                                        <p class="text-center"> @{{ category.name }} </p>
-                                    </label>
-                                </div>
-                            </div>
-                            <a id="prev" class="carousel-control-prev" href="#carousel-example" role="button" data-slide="prev">
-                              <i class="fa fa-chevron-left" aria-hidden="true"></i>
-                            </a>
-                            <a id="next" class="carousel-control-next" href="#carousel-example" role="button" data-slide="next">
-                              <i class="fa fa-chevron-right" aria-hidden="true"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<div id="app">
 
-            {{-- restaurants list --}}
-            <div class="col-lg-12 ristoranti">
-                <h3 class="text-center" :class="categories_rest.length > 0 ? 'd-block':'d-none'">La tua ricerca ha trovato: @{{restaurants.length}} ristoranti</h3>
-                <div class="row mt-4">
-                  <div class="col-lg-4 col-md-6 mb-4" v-for="restaurant in restaurants" >
-                    <a :href="'restaurant/' + restaurant.slug" class="text">
-                      <div class=" h-100 relative">
-                        <img class="card-img-top card-foto" :src="`storage/${restaurant.image}`" alt="">
-                        <div class="card-body">
-                          <h4 class="card-title">
-                            <p class="text-center">@{{ restaurant.name }}</p>
-                            <li v-for="category in restaurant.categories">@{{category.name}}</li>
-                          </h4>
-                        </div>
+  <div class="container">
+      <div class="row">
+          <div class="col-lg-12 mb-4">
+              <h3 class="mt-2 text-center">Scegli tra queste categorie di cucina</h3>
+              
+              <!-- Top content -->
+              <div class="top-content">
+                  <div class="container-fluid">
+                      <div id="carousel-example" class="carousel slide" data-ride="carousel">
+                          <div class="carousel-inner row w-100 mx-auto" role="listbox">
+                              <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3" :class="index==0 ? 'active' : '' " v-for="(category, index) in categories">
+                                  <input class="form-check-input d-none" type="checkbox" :value="category.id" :id="'category'+index" v-model="categories_rest">
+                                  <label class="form-check-label cat_label" :for="'category' + index">
+                                      <img src="//placehold.it/600x400/000/fff?text=1" class="img-fluid mx-auto d-block" alt="img1">
+                                      <p class="text-center"> @{{ category.name }} </p>
+                                  </label>
+                              </div>
+                          </div>
+                          <a id="prev" class="carousel-control-prev" href="#carousel-example" role="button" data-slide="prev">
+                            <i class="fa fa-chevron-left" aria-hidden="true"></i>
+                          </a>
+                          <a id="next" class="carousel-control-next" href="#carousel-example" role="button" data-slide="next">
+                            <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                          </a>
                       </div>
-                    </a>
                   </div>
+              </div>
+          </div>
+
+          {{-- restaurants list --}}
+          <div class="col-lg-12 ristoranti">
+              <h3 class="text-center" :class="categories_rest.length > 0 ? 'd-block':'d-none'">La tua ricerca ha trovato: @{{restaurants.length}} ristoranti</h3>
+              <div class="row mt-4">
+                <div class="col-lg-4 col-md-6 mb-4" v-for="restaurant in restaurants" >
+                  <a :href="'restaurant/' + restaurant.slug" class="text">
+                    <div class=" h-100 relative">
+                      <img class="card-img-top card-foto" :src="`storage/${restaurant.image}`" alt="">
+                      <div class="card-body">
+                        <h4 class="card-title">
+                          <p class="text-center">@{{ restaurant.name }}</p>
+                          <li v-for="category in restaurant.categories">@{{category.name}}</li>
+                        </h4>
+                      </div>
+                    </div>
+                  </a>
                 </div>
               </div>
-            {{-- restaurants list --}}
+            </div>
+          {{-- restaurants list --}}
 
-        </div>
-    </div>
+      </div>
+  </div>
+</div>
         
 
 @endsection
