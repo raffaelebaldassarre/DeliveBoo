6f470d42fbb4beb73c0a2b9441b3c01708086e6c
williamtruppi <williamt@hotmail.it>
2021-03-29 10:59:09 +0200

    other style admin-restaurants

--- resources/views/Admin/Restaurants/show.blade.php
+++ resources/views/Admin/Restaurants/show.blade.php
@@ -8,26 +8,30 @@
 
 @section('content')
 
-<div id="show_rest_container">
-    <div class="images d-flex">
-        <img src="{{asset('storage/' . $restaurant->image)}}" alt="" style="height: 250px">
-    </div>
-    <div id="single_rest_show">
-        <h1>Nome Attività: {{$restaurant->name}}</h1>
+<div id="show_rest_container" class="d-flex">
+    <div class="row">
+        <div class="col-xs-12 col-md-12 col-lg-6 images text-center">
+            <img src="{{asset('storage/' . $restaurant->image)}}" alt="" style="height: 250px">
+        </div>
     
+        <div class="col-xs-12 col-md-12 col-lg-6" id="single_rest_show">
+            <h1>Nome Attività: {{$restaurant->name}}</h1>
         
-        <h3>Indirizzo: {{$restaurant->address}}</h3>
-        <h3>Numero di telefono: {{$restaurant->phone_number}}</h3>
+            
+            <h3>Indirizzo: {{$restaurant->address}}</h3>
+            <h3>Numero di telefono: {{$restaurant->phone_number}}</h3>
+            
+            <h3> Categorie:
+                @foreach ($restaurant->categories as $category)
+                     {{$category->name}}
+                @endforeach
+            </h3>
         
-        <h3> Categorie:
-            @foreach ($restaurant->categories as $category)
-                 {{$category->name}}
-            @endforeach
-        </h3>
-    
-        <a href="{{route('admin.restaurants.edit', ['restaurant' => $restaurant->slug])}}" class="btn btn-warning"><i class="fas fa-pencil-ruler fa-xs fa-fw"></i></a>
-        <a href="{{route('admin.dishes.index', ['slug' => $restaurant->slug])}}" class="btn btn-warning"><i class="fas fa-pencil-ruler fa-xs fa-fw"></i>Visualizza i piatti del tuo ristorante</a>
+            <a href="{{route('admin.restaurants.edit', ['restaurant' => $restaurant->slug])}}" class="btn btn-warning"><i class="fas fa-pencil-ruler fa-xs fa-fw"></i></a>
+            <a href="{{route('admin.dishes.index', ['slug' => $restaurant->slug])}}" class="btn btn-warning"><i class="fas fa-pencil-ruler fa-xs fa-fw"></i>Visualizza i piatti del tuo ristorante</a>
+        </div>
     </div>
+
 </div>
     
 
