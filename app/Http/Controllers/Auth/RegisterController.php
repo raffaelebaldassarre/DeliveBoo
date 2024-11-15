ebfdecb27c0fec0e0cfd1b18f9dad62cc05e51c5
Raffaele Baldassarre <>
2024-11-15 14:23:20 +0100

    fix

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,7 +53,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'vat' => ['required', 'regex:^(IT)[0-9]{11}$^']
+            'vat' => ['required', 'regex:^(IT)[0-9]{11}$']
         ]);
     }
 
